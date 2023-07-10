c449968c6ee8a8b775a682de1083a9ee67a2f901
ryooout <ryota01010104@gmail.com>
2023-07-10 13:34:56 +0900

    敵とプレイヤーのHPやSPが減るようなスクリプト

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,59 +1,74 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     [SerializeField] GameSystemStatus _player;
     [SerializeField] GameSystemStatus _enemy;
     float _turnSpan;
-    float _skillSpan;
     bool _isPlayerTurn;
     bool _isSkill;
-    int _reflection;
     int _escapeRand;
+    [SerializeField] Text _logText;
+    [SerializeField] Transform _parent;
+    [SerializeField] Slider _playerSpSlider;
     void Start()
     {
         _isPlayerTurn = true;
         _isSkill = false;
-    }  
+        _playerSpSlider.value = _player.Sp;
+        _playerSpSlider.maxValue = _player.Sp;
+    }
     void Update()
     {
-        if(!_isPlayerTurn)
+        if (!_isPlayerTurn)
         {
             _turnSpan += Time.deltaTime;
-            if(_turnSpan>=5.0f)
+            if (_turnSpan >= 5.0f)
             {
-                if(_isSkill)
+                if (_isSkill)
                 {
                     _turnSpan = 0;
-                    _isPlayerTurn = true;
-                    if (_reflection<=3)
-                    {
-                        _skillSpan += Time.deltaTime;
-                        if (_skillSpan >= 0.5f)
-                        {
-                            //“G©g,UŒ‚‚ğó‚¯‚é
-                            _enemy.RandDamageEnemy = Random.Range(1, 5);
-                            _enemy.Damage(_enemy.RandDamageEnemy);
-                            _reflection++;
-                        }
-                    }
-                    else
-                    {
-                        _reflection = 0;
-                    }
+                    //“G©g,UŒ‚‚ğó‚¯‚é
+                    _enemy.RandDamageEnemy = Random.Range(1, 5);
+                    _enemy.Damage(_enemy.RandDamageEnemy);
+                    _logText.text = $"“G‚É{_enemy.RandDamageEnemy}‚Ìƒ_ƒ[ƒW‚ğ—^‚¦‚½!!\n";
+                    Text tx = Instantiate(_logText);
+                    tx.transform.SetParent(_parent, false);
+                    Debug.Log(_enemy.RandDamageEnemy);
+                    //“G©g,UŒ‚‚ğó‚¯‚é
+                    _enemy.RandDamageEnemy = Random.Range(1, 5);
+                    _enemy.Damage(_enemy.RandDamageEnemy);
+                    _logText.text = $"“G‚É{_enemy.RandDamageEnemy}‚Ìƒ_ƒ[ƒW‚ğ—^‚¦‚½!!\n";
+                    Text tx1 = Instantiate(_logText);
+                    tx1.transform.SetParent(_parent, false);
+                    Debug.Log(_enemy.RandDamageEnemy);
+                    //“G©g,UŒ‚‚ğó‚¯‚é
+                    _enemy.RandDamageEnemy = Random.Range(1, 5);
+                    _enemy.Damage(_enemy.RandDamageEnemy);
+                    _logText.text = $"“G‚É{_enemy.RandDamageEnemy}‚Ìƒ_ƒ[ƒW‚ğ—^‚¦‚½!!\n";
+                    Text tx2 = Instantiate(_logText);
+                    tx2.transform.SetParent(_parent, false);
+                    Debug.Log(_enemy.RandDamageEnemy);
+                    Destroy(tx, 2.0f);
+                    Destroy(tx1, 2.0f);
+                    Destroy(tx2, 2.0f);
                 }
                 else if (!_isSkill)
                 {
                     _turnSpan = 0;
-                    _isPlayerTurn = true;
                     //ƒvƒŒƒCƒ„[‚ªƒ_ƒ[ƒW‚ğó‚¯‚é
                     _enemy.RandDamageEnemy = Random.Range(1, 5);
                     _player.Damage(_enemy.RandDamageEnemy);
+                    _logText.text = $"“G‚©‚ç{_enemy.RandDamageEnemy}‚Ìƒ_ƒ[ƒW‚ğó‚¯‚½!!\n";
+                    Text tx3 = Instantiate(_logText);
+                    tx3.transform.SetParent(_parent, false);
                     Debug.Log(_enemy.RandDamageEnemy);
+                    Destroy(tx3, 2.0f);
                 }
-            }           
+                _isPlayerTurn = true;
+            }
         }
     }
     public void PlayerAttackButton()
@@ -64,13 +79,19 @@ public class GameManager : MonoBehaviour
             //“G‚ªƒ_ƒ[ƒW‚ğó‚¯‚é
             _player.RandDamagePlayer = Random.Range(2, 10);
             _enemy.Damage(_player.RandDamagePlayer);
+            _logText.text = $"“G‚É{_player.RandDamagePlayer}‚Ìƒ_ƒ[ƒW‚ğ—^‚¦‚½!!\n";
+            Text attackText = Instantiate(_logText);
+            attackText.transform.SetParent(_parent, false);
+            Destroy(attackText, 2.0f);
             Debug.Log(_player.RandDamagePlayer);
         }
     }
     public void PlayerSkillButton()
     {
-        if(_isPlayerTurn)
+        if (_isPlayerTurn)
         {
+            _player.Sp -= 5;
+            _playerSpSlider.value = _player.Sp;
             _escapeRand = Random.Range(1, 10);
             if (_escapeRand <= 8)
             {
